8cd983ced90b375e7793a22d9fc80a9c3847e157
Jonathan JK Stoltman <code@opioidpolicy.org>
2025-08-25 16:58:13 -0400

    beta 0.3.9 vercel errors

--- src/app/components/ScrollToTop.tsx
+++ src/app/components/ScrollToTop.tsx
@@ -1,18 +1,17 @@
 // src/app/components/ScrollToTop.tsx
 'use client';
 import { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 export default function ScrollToTop() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     // Only scroll to top if there's no hash in the URL
     if (!window.location.hash) {
       window.scrollTo(0, 0);
     }
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null;
 }
